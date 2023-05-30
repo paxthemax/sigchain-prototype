70dee9d7f9805fb1183ea228b5088066c556a1b8
Pavle Batuta <pavle@batuta.xyz>
2023-05-30 12:03:05 +0200

    Initial commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,38 @@
+import hre from "hardhat";
+
+import { VERBOSE } from "../hardhat.config";
+import { Counter, Counter__factory } from "../types";
+import { deployWait } from "./utils";
+import { GasOptions } from "./types";
+import { Wallet } from "ethers";
+
+// --- Helper functions for deploying contracts ---
+
+// Also adds them to hardhat-tracer nameTags, which gives them a trackable name
+// for events when `npx hardhat test --logs` is used.
+
+// deployCounter deploys the Counter contract with an initial count value.
+export async function deployCounter(
+    wallet: Wallet,
+    gasOpts?: GasOptions,
+    initCount?: number,
+): Promise<Counter> {
+    if (initCount === undefined) {
+        initCount = 0;
+    }
+
+    let counterContract: Counter;
+    const counter: Counter__factory = await hre.ethers.getContractFactory(`Counter`, wallet);
+    counterContract = await deployWait(
+        counter.deploy(initCount, {
+            maxFeePerGas: gasOpts?.maxFeePerGas,
+            maxPriorityFeePerGas: gasOpts?.maxPriorityFeePerGas,
+            gasLimit: gasOpts?.gasLimit,
+        }),
+    );
+
+    if (VERBOSE) console.log(`Counter: ${counterContract.address}`);
+    hre.tracer.nameTags[counterContract.address] = `Counter`;
+
+    return counterContract;
+}
